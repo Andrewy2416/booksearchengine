91715786840552197d098297e5c4c59a4ac644bc
Andrewy2416 <andrewy2416@gmail.com>
2022-02-19 03:27:22 -0800

    added server definition

--- server/server.js
+++ server/server.js
@@ -12,6 +12,11 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 server.start().then(() => {
   server.applyMiddleware({ app });
 });
