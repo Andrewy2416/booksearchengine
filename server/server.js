7cee9e1e75891b1e09faef5d6b5f74c4cdfbe7f9
Andrewy2416 <andrewy2416@gmail.com>
2022-02-19 02:43:12 -0800

    fixed server issue

--- server/server.js
+++ server/server.js
@@ -12,13 +12,18 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
-//apply apollo server
-server.applyMiddleware({app});
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
