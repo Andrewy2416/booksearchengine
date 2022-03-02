aa8007fb4e9df1a551e707481dea975616391824
Andrewy2416 <andrewy2416@gmail.com>
2022-03-02 02:27:46 -0800

    updated graphQL queries

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,61 +1,77 @@
 import gql from "graphql-tag";
 
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
-  }
-`;
-
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
-export const SAVE_BOOK = gql`
-  mutation saveBook($book: SavedBookInput!) {
-    saveBook(book: $book) {
-      username
-      email
-      bookCount
-      savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
-        title
-      }
+export const USER_LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
+`;
+
+export const ADD_BOOK = gql`
+    mutation addBook(
+        $userId: ID!
+        $authors: [String]!
+        $description: String!
+        $bookId: String!
+        $image: String!
+        $link: String!
+        $title: String!
+    ) {
+        addBook(
+            userId: $userId
+            authors: $authors
+            description: $description
+            bookId: $bookId
+            image: $image
+            link: $link
+            title: $title
+        ) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                image
+                link
+                title
+                bookId
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: String!) {
-    removeBook(bookId: $bookId) {
-      username
-      email
-      bookCount
-      savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
-        title
-      }
+    mutation removeBook($userId: String!, $bookId: String!) {
+        removeBook(userId: $userId, bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                image
+                link
+                title
+                bookId
+            }
+        }
     }
-  }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,39 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
-export const GET_ME = gql`
-  {
-    me {
-      _id
-      username
-      email
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        description
-        title
-        image
-        link
-      }
+export const GET_USERS = gql`
+    query Query {
+        users {
+            _id
+            username
+            email
+            password
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
     }
-  }
+`;
+
+export const GET_USER = gql`
+    query Query($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username
+            email
+            password
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
 `;
